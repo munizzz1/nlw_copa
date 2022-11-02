c7433ddf8e03061f5835629a861f25fcd9fc5f02
Jhonatan Muniz <jhonatanmuniz123@gmail.com>
2022-11-02 17:53:52 -0300

    Update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import Fastify from 'fastify';
+import ShortUniqueId from 'short-unique-id';
 import cors from '@fastify/cors';
+import Fastify from 'fastify';
+import { z } from 'zod';
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -19,6 +21,38 @@ async function bootstrap() {
         const count = await prisma.pool.count();
 
         return { count };
+    });
+
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count();
+
+        return { count };
+    });
+
+    fastify.get('/guesses/count', async () => {
+        const count = await prisma.guess.count();
+
+        return { count };
+    });
+
+    fastify.post('/pools', async (request, response) => {
+        const createPollBody = z.object({
+            title: z.string(),
+        });
+        
+        const { title } = createPollBody.parse(request.body);
+
+        const generate = new ShortUniqueId({ length: 6 });
+        const code = String(generate()).toUpperCase();
+
+        await prisma.pool.create({
+            data: {
+                title,
+                code
+            }
+        });
+
+        return response.status(201).send({ code });
     });
 
     await fastify.listen({ port: 3333 , host: '0.0.0.0'});
